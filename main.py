7dcd1b33c5a891b754beaf829a3a6a829a734b8e
vitorHugo87 <vitorpaduan87@gmail.com>
2024-10-11 12:30:43 -0300

    Funções mostrarMenu e pedirComando

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('Hello World!')
+import os
+
+def mostrarMenu(menu):
+    os.system('cls')
+    if menu == 'inicio':
+        print(' Menu '.center(20, '-'))
+        print('1 - Cadastro')
+        print('2 - Relatorios')
+        print('3 - Sair')
+
+def pedirComando(opcoes):
+    while True:
+        comando = input('Comando: ')
+        if comando.isnumeric():
+            comando = int(comando)
+            if 0 < comando <= opcoes: return comando
+            else: print('-=- Erro! Valor Invalido digitado! -=-')
+        else: print('-=- Erro! Comando não pode conter letras! -=-')
+
+while True:
+    pedirComando(3)
