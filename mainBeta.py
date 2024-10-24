2c2523671172316220a0b05fbf421153d57bfff7
vitorHugo87 <vitorpaduan87@gmail.com>
2024-10-24 16:24:40 -0300

    Criação da Versão Beta

---
+++ mainBeta.py
@@ -0,0 +1,25 @@
+alunos = [
+    {'ra': 1, 'senha': '123456', 'nome': 'Heitor Pereira', 'idade': 2, 'mae': 'Heloisa Pereira', 'turma': 'A', 'notas': [85, 75], 'media': 80},
+    {'ra': 2, 'senha': '654321', 'nome': 'Laura Souza', 'idade': 3, 'mae': 'Mariana Souza', 'turma': 'A', 'notas': [65, 45], 'media': 55},
+    {'ra': 3, 'senha': '112233', 'nome': 'Pedro Oliveira', 'idade': 2, 'mae': 'Ana Oliveira', 'turma': 'A', 'notas': [70, 60], 'media': 65},
+    {'ra': 10, 'senha': '789123', 'nome': 'Mariana Silva', 'idade': 3, 'mae': 'Luciana Silva', 'turma': 'A', 'notas': [85, 80], 'media': 82.5},
+    
+    {'ra': 4, 'senha': '445566', 'nome': 'Lucas Lima', 'idade': 1, 'mae': 'Fernanda Lima', 'turma': 'B', 'notas': [50, 40], 'media': 45},
+    {'ra': 5, 'senha': '778899', 'nome': 'Sofia Santos', 'idade': 2, 'mae': 'Bruna Santos', 'turma': 'B', 'notas': [90, 80], 'media': 85},
+    {'ra': 6, 'senha': '123789', 'nome': 'Rafael Ferreira', 'idade': 1, 'mae': 'Juliana Ferreira', 'turma': 'B', 'notas': [60, 50], 'media': 55},
+    {'ra': 11, 'senha': '987321', 'nome': 'Felipe Lopes', 'idade': 2, 'mae': 'Carla Lopes', 'turma': 'B', 'notas': [70, 60], 'media': 65},
+    
+    {'ra': 7, 'senha': '987654', 'nome': 'Isabela Martins', 'idade': 0, 'mae': 'Cláudia Martins', 'turma': 'C', 'notas': [95, 85], 'media': 90},
+    {'ra': 8, 'senha': '321987', 'nome': 'Giovanna Costa', 'idade': 1, 'mae': 'Patrícia Costa', 'turma': 'C', 'notas': [80, 75], 'media': 77.5},
+    {'ra': 9, 'senha': '654123', 'nome': 'Arthur Ribeiro', 'idade': 2, 'mae': 'Roberta Ribeiro', 'turma': 'C', 'notas': [50, 45], 'media': 47.5},
+    {'ra': 12, 'senha': '321654', 'nome': 'Ana Clara Lima', 'idade': 1, 'mae': 'Sônia Lima', 'turma': 'C', 'notas': [55, 50], 'media': 52.5}
+]
+
+profs = [
+    {'id': 1, 'email': 'julio@gmail.com', 'senha': 'julio123', 'nome': 'Julio Vargas', 'turmas': ['A', 'C'], 'salario': 2500.0},
+    {'id': 2, 'email': 'helena@gmail.com', 'senha': 'helena123', 'nome': 'Helena Silva', 'turmas': ['B', 'C'], 'salario': 2750.0},
+    {'id': 3, 'email': 'ana@gmail.com', 'senha': 'ana123', 'nome': 'Ana Oliveira', 'turmas': ['A', 'B'], 'salario': 2600.0},
+    {'id': 4, 'email': 'carlos@gmail.com', 'senha': 'carlos123', 'nome': 'Carlos Souza', 'turmas': ['A', 'B', 'C'], 'salario': 2900.0},
+    {'id': 5, 'email': 'maria@gmail.com', 'senha': 'maria123', 'nome': 'Maria Santos', 'turmas': ['A', 'B'], 'salario': 2400.0},
+    {'id': 6, 'email': 'joao@gmail.com', 'senha': 'joao123', 'nome': 'João Ribeiro', 'turmas': ['C'], 'salario': 2700.0}
+]
